ebbf25ebd642ccf8f3a3bb221b7438f9801fa1b4
vinnu <vinnidasarla21@gmail.com>
2018-04-15 17:46:11 +0530

    My first comit
    Created initial project and chekc in the code

---
+++ ratingbar/app/src/main/java/com/example/vinitha/ratingbar/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.vinitha.ratingbar;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar mRatingBar;
+    TextView mRatingScale;
+    EditText mFeedback;
+    Button mSendFeedback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, NotificationActivityNew.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("mginfomatics")
+                .setContentText("hi helo iam from mginfomatics")
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mRatingScale = (TextView) findViewById(R.id.tvRatingScale);
+        mFeedback = (EditText) findViewById(R.id.etFeedback);
+        mSendFeedback = (Button) findViewById(R.id.btnSubmit);
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Awesome. I love it");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+
+        mSendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFeedback.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please fill in feedback text box", Toast.LENGTH_LONG).show();
+                } else {
+                    mFeedback.setText("");
+                    mRatingBar.setRating(0);
+                    Toast.makeText(MainActivity.this, "Thank you for sharing your feedback", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    }
+
+
---
+++ ratingbar/app/src/main/java/com/example/vinitha/ratingbar/NotificationActivityNew.java
@@ -0,0 +1,4 @@
+package com.example.vinitha.ratingbar;
+
+class NotificationActivityNew {
+}
